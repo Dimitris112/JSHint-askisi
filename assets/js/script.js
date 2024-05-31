9c81ba013d91221b164184d37ec4e015be8969df
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-05-31 12:47:51 +0000

    liges allages

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,21 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 
 
 document.getElementById('status').addEventListener('click', e => getStatus(e));
+document.getElementById('submit').addEventListener('submit', e => postForm(e));
 
+async function postForm(e) {
+    const form = new FormData(document.getElementById('checksform'));
+
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY
+        },
+        body: form
+    })
+
+}
 
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
