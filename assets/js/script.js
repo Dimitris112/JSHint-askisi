19331ab167988644a21ef93adf7d72b58cf5ebf6
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-05-31 12:28:24 +0000

    kales allages sto api k diorthwsi tou typo kai data.expiry

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
-Q6gryoOG3O13xcvX6k1Hhe9UpLA
+const API_KEY = "Q6gryoOG3O13xcvX6k1Hhe9UpLA";
+
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+
+
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+
+document.getElementById('status').addEventListener('click', e => getStatus(e));
+
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+}
